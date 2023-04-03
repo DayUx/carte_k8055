683b65715eb361045607b39bef051102a40f6832
Paul Coignac <coignac.e2001154@etud.univ-ubs.fr>
2023-04-03 09:30:46 +0200

    c réparé

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.example.Pk_Process.Lancement;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        Lancement lancement = new Lancement();
+        lancement.Lance();
+    }
+}
